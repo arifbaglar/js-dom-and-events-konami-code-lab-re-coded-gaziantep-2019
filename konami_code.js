58a7afbe21e0ed2ff38154b3399050964f7a9370
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-07-28 00:58:39 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,22 +2,19 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index=0;
 function init(e) {
   
-  document.body.addEventListener('keydown', arif);
-  function arif(e){
-  const key = parseInt(e.which);
- 
-  if (key === code[index]) {console.log(key)
-    index++;
- 
-    if (index === code.length) {
-      alert("tebrikler!");
- 
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
+   document.body.addEventListener('keydown', onKeyDownHandler);
 
 }
+function onKeyDownHandler(e) {
+ const key = parseInt(e.detail || e.which);
+  if (key === code[index]) {
+   index++;
 
+    if (index === code.length) {
+       alert("Hurray!");
+       index = 0;
+   }
+ } else {
+   index = 0;
+  }
 }
